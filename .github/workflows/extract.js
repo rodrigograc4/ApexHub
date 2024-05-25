8d376205e5cdf4ec1f3ea7b98005b5365cf8f0d9
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-05-25 20:36:24 +0100

    steal try

---
+++ .github/workflows/extract.js
@@ -0,0 +1,18 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://sportsonline.gl/prog.txt');
+
+  // Extrair todo o HTML da página
+  const htmlContent = await page.evaluate(() => document.documentElement.outerHTML);
+
+  // Escrever o HTML extraído em um arquivo
+  fs.writeFileSync('conteudo.html', htmlContent);
+
+  console.log('HTML extraído e salvo em conteudo.html');
+
+  await browser.close();
+})();
