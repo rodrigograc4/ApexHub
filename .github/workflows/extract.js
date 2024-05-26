5db763aadc52ac6cfb470fc2d4c6fb1eb35c1795
rodrigograc4 <rodrigomgraca@ua.pt>
2024-05-26 01:01:53 +0100

    final

--- .github/workflows/extract.js
+++ .github/workflows/extract.js
@@ -10,7 +10,7 @@ const fs = require('fs');
   const htmlContent = await page.content();
 
   // Escrever o HTML extraído em um arquivo
-  fs.writeFileSync('.github/workflows/conteudo.html', htmlContent);
+  fs.writeFileSync('conteudo.html', htmlContent);
 
   console.log('HTML extraído e salvo em conteudo.txt');
 
